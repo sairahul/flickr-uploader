9bd5d0ff2f55eb16b4f59ba82855ab90101c3fa2
Sai Rahul <sairahul@gmail.com>
2012-01-22 20:02:28 +0530

    cleaned up code, added option to put directory name as tag

--- flickr_uploader.py
+++ flickr_uploader.py
@@ -7,6 +7,7 @@ import glob
 import sqlite3
 import logging
 import traceback
+from optparse import OptionParser
 
 import flickrapi
 
@@ -36,7 +37,41 @@ def cursor(_dir):
     curr.execute("create table if not exists flickr_images (file text primary key, status text)")
     return conn, curr
 
-def upload_to_flickr(src_dirs):
+def is_uploaded(curr, _file):
+    curr.execute("select status from flickr_images where file = ?", (_file,))
+    result = curr.fetchall()
+
+    status = (result and result[0] and result[0][0]) or ""
+    if status.lower() == "ok":
+        logger.debug("Uploaded before. skipping %s"%(_file))
+        return True
+
+    return False
+
+def upload_file(curr, conn, flickr, _file, update_tag, tag):
+    rsp = None
+    time_taken = 0
+    try:
+        t1 = time.time()
+        if update_tag:
+            rsp = flickr.upload(_file, tags=tag)
+        else:
+            rsp = flickr.upload(_file)
+
+        time_taken = time.time() - t1
+    except:
+        logger.error(''.join(traceback.format_exception(*sys.exc_info()[:3])))
+    else:
+        if rsp and rsp.attrib and rsp.attrib["stat"].lower() == "ok":
+            curr.execute("insert or replace into flickr_images (file, status) values (?, ?)",(_file, "ok"))
+            conn.commit()
+            return True, time_taken
+        else:
+            logger.error("Couldn't upload: %s :(, - rsp: %s "%(_file, rsp.attrib))
+
+    return False, time_taken
+
+def upload_to_flickr(update_tag, src_dirs):
     flickr = flickrapi.FlickrAPI(API_KEY, API_SECRET)
     (token, frob) = flickr.get_token_part_one(perms='write')
     if not token: raw_input("Press ENTER after you authorized this program")
@@ -52,39 +87,26 @@ def upload_to_flickr(src_dirs):
 
         _uploaded = _errors = _skipped = total_time = 0
 
+        tag = _dir.split("/")[-1]
+        tag = repr(tag.strip())
         for _file in files_to_upload:
             if not is_image(_file):
+                _skipped  += 1
                 logger.error("Not an image. skipping .. %s"%(_file))
-                _skipped  += 1
-                continue
 
-            curr.execute("select status from flickr_images where file = ?", (_file,))
-            result = curr.fetchall()
-
-            status = (result and result[0] and result[0][0]) or ""
-            if status.lower() == "ok":
+            if is_uploaded(curr, _file):
                 _skipped  += 1
-                logger.debug("Uploaded before. skipping %s"%(_file))
                 continue
 
             logger.debug("Uploading %s ..."%(_file))
 
-            rsp = {}
-            try:
-                t1 = time.time()
-                rsp = flickr.upload(_file)
-                total_time += (time.time() - t1)
-            except:
-                logger.error(''.join(traceback.format_exception(*sys.exc_info()[:3])))
-                _errors += 1
+            uploaded, time_taken = upload_file(curr, conn, flickr, _file, update_tag, tag)
+            total_time += time_taken
+            if uploaded:
+                _uploaded += 1
             else:
-                if rsp and rsp.attrib and rsp.attrib["stat"].lower() == "ok":
-                    _uploaded += 1
-                    curr.execute("insert or replace into flickr_images (file, status) values (?, ?)",(_file, "ok"))
-                    conn.commit()
-                else:
-                    logger.error("Couldn't upload: %s :(, - rsp: %s "%(_file, rsp.attrib))
-                    _errors += 1
+                _errors += 1
+
 
         _avg = (_uploaded and (total_time / float(_uploaded))) or 0
         logger.info("Dir Completed: %s"%(_dir))
@@ -94,6 +116,23 @@ def upload_to_flickr(src_dirs):
         conn.close()
 
 if __name__=="__main__":
-    src_dirs = sys.argv[1:]
-    upload_to_flickr(src_dirs)
+    parser = OptionParser()
+    parser.add_option("-t", "--tags",
+                     action="store_true", default=False,
+                     help="Tag all the photos with the directory name.")
+
+    parser.add_option("-f", "--file", dest="filename",
+                     help="File contains list of directories to upload.",
+                     metavar="FILE")
+    (options, args) = parser.parse_args()
+
+    src_dirs = args
+    if options.filename:
+        if not os.path.exists(options.filename):
+            raise IOError("No such file or directory: %s"%options.filename)
+
+        dirs = [line.strip() for line in open(options.filename) if line.strip()]
+        src_dirs.extend(dirs)
+
+    upload_to_flickr(options.tags, src_dirs)
 
